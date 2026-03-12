80b05e0e8ca36330b6e6eb1a3b940dee17f84d6d
Camilovillanueva <camilo.villanueva@udea.edu.co>
2026-03-11 21:11:43 -0500

    Definicion y generacion de mascaras de cada piezas centralizadas en el tablero

--- DesafioI/main.cpp
+++ DesafioI/main.cpp
@@ -2,13 +2,8 @@
 
 using namespace std;
 
-unsigned char* crearTablero(int ancho, int alto, int &bytesPorFila) {
-
-    if (ancho < 8 || alto < 8 || ancho % 8 != 0) {
-        cout << "Dimensiones invalidas" << endl;
-        return NULL;
-    }
-
+unsigned char* crearTablero(int ancho, int alto, int &bytesPorFila)
+{
     bytesPorFila = ancho / 8;
 
     unsigned char* tablero = new unsigned char[alto * bytesPorFila];
@@ -16,25 +11,22 @@ unsigned char* crearTablero(int ancho, int alto, int &bytesPorFila) {
     return tablero;
 }
 
-void inicializarTablero(unsigned char* tablero, int alto, int bytesPorFila) {
-
-    for (int i = 0; i < alto * bytesPorFila; i++) {
+void inicializarTablero(unsigned char* tablero, int alto, int bytesPorFila)
+{
+    for(int i = 0; i < alto * bytesPorFila; i++)
         tablero[i] = 0;
-    }
-
 }
 
-void imprimirTablero(unsigned char* tablero, int ancho, int alto, int bytesPorFila) {
-
-    for (int y = 0; y < alto; y++) {
-
-        for (int x = 0; x < ancho; x++) {
-
+void imprimirTablero(unsigned char* tablero, int ancho, int alto, int bytesPorFila)
+{
+    for(int y = 0; y < alto; y++)
+    {
+        for(int x = 0; x < ancho; x++)
+        {
             int indice = y * bytesPorFila + (x / 8);
-
             int bit = 7 - (x % 8);
 
-            if ((tablero[indice] >> bit) & 1)
+            if((tablero[indice] >> bit) & 1)
                 cout << "#";
             else
                 cout << ".";
@@ -42,30 +34,156 @@ void imprimirTablero(unsigned char* tablero, int ancho, int alto, int bytesPorFi
 
         cout << endl;
     }
-
 }
 
-int main() {
+void crearPieza(unsigned char pieza[4], int tipo)
+{
+    for(int i = 0; i < 4; i++)
+        pieza[i] = 0;
 
+    switch(tipo)
+    {
+    case 0: // I
+        pieza[0] = 0b11110000;
+        break;
+
+    case 1: // O
+        pieza[0] = 0b11000000;
+        pieza[1] = 0b11000000;
+        break;
+
+    case 2: // T
+        pieza[0] = 0b11100000;
+        pieza[1] = 0b01000000;
+        break;
+
+    case 3: // S
+        pieza[0] = 0b01100000;
+        pieza[1] = 0b11000000;
+        break;
+
+    case 4: // Z
+        pieza[0] = 0b11000000;
+        pieza[1] = 0b01100000;
+        break;
+
+    case 5: // J
+        pieza[0] = 0b10000000;
+        pieza[1] = 0b11100000;
+        break;
+
+    case 6: // L
+        pieza[0] = 0b00100000;
+        pieza[1] = 0b11100000;
+        break;
+    }
+}
+
+int obtenerAnchoPieza(int tipo)
+{
+    switch(tipo)
+    {
+    case 0: return 4; // I
+    case 1: return 2; // O
+    case 2: return 3; // T
+    case 3: return 4; // S
+    case 4: return 4; // Z
+    case 5: return 3; // J
+    case 6: return 3; // L
+    default: return 4;
+    }
+}
+
+void imprimirPieza(unsigned char pieza[4])
+{
+    for(int y = 0; y < 4; y++)
+    {
+        for(int bit = 7; bit >= 0; bit--)
+        {
+            if((pieza[y] >> bit) & 1)
+                cout << "#";
+            else
+                cout << ".";
+        }
+
+        cout << endl;
+    }
+}
+
+int generarPieza()
+{
+    static int pieza = 0;
+
+    pieza++;
+
+    if(pieza == 7)
+        pieza = 0;
+
+    return pieza;
+}
+
+void colocarPieza(unsigned char* tablero, unsigned char pieza[4],
+                  int x, int y, int bytesPorFila, int alto)
+{
+    for(int fila = 0; fila < 4; fila++)
+    {
+        if (y + fila >= alto) break;
+
+        int baseIndice = (y + fila) * bytesPorFila;
+        int byteIndex = x / 8;
+        int bitOffset = x % 8;
+
+        if (byteIndex >= bytesPorFila) continue;
+
+        unsigned char parteIzquierda = pieza[fila] >> bitOffset;
+        unsigned char parteDerecha = pieza[fila] << (8 - bitOffset);
+
+        tablero[baseIndice + byteIndex] |= parteIzquierda;
+
+        if (byteIndex + 1 < bytesPorFila)
+        {
+            tablero[baseIndice + byteIndex + 1] |= parteDerecha;
+        }
+    }
+}
+
+int main()
+{
     int ancho;
     int alto;
 
-    cout << "Ingrese el ancho (multiplo de 8): ";
-    cin >> ancho;
+    do
+    {
+        cout << "Ingrese el ancho (multiplo de 8): ";
+        cin >> ancho;
 
-    cout << "Ingrese el alto: ";
-    cin >> alto;
+        cout << "Ingrese el alto: ";
+        cin >> alto;
+
+        if(ancho < 8 || alto < 8 || ancho % 8 != 0)
+            cout << "Dimensiones invalidas\n";
+
+    } while(ancho < 8 || alto < 8 || ancho % 8 != 0);
 
     int bytesPorFila;
 
     unsigned char* tablero = crearTablero(ancho, alto, bytesPorFila);
 
-    if (tablero == NULL)
-        return 0;
-
     inicializarTablero(tablero, alto, bytesPorFila);
 
-    cout << endl << "Tablero creado:" << endl;
+    unsigned char pieza[4];
+
+    int tipo = generarPieza();
+
+    crearPieza(pieza, tipo);
+
+    int anchoPieza = obtenerAnchoPieza(tipo);
+    int x = (ancho - anchoPieza) / 2;
+    int y = 0;
+
+    colocarPieza(tablero, pieza, x, y, bytesPorFila, alto);
+
+    cout << "\nTablero:\n";
 
     imprimirTablero(tablero, ancho, alto, bytesPorFila);
 
